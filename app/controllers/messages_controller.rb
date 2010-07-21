452a390acb0455fd20395290e6d0559f748a9cb8
func09 <mitsuru.haga@gmail.com>
2010-07-21 20:52:54 +0900

    -

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,15 @@
+class MessagesController < ApplicationController
+  def index
+  end
+  # ここから追加
+  def create
+    if current_user.twitter.post('', :status => "偉大なるHelloWorld")
+      flash[:success] = "おめでとう！偉大なるHelloWorldは成功した。"
+      redirect_to root_path
+    else
+      flash[:error] = "残念だが、偉大なるHelloWorldは失敗に終わった。"
+      render :action => 'index'
+    end
+  end
+  # ここまで追加
+end
